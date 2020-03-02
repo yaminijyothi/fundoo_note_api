96d429cdf5dac46feeca7bca87ad94d550dadf1b
yaminijyothi <yamini0234@gmail.com>
2020-03-02 18:33:16 +0530

    [yaminijyothi] refactor. fundoo note application added.

--- src/main/java/com/bridgelabz/fundoonotes/FundoonotesApplication.java
+++ src/main/java/com/bridgelabz/fundoonotes/FundoonotesApplication.java
@@ -1,5 +1,7 @@
 package com.bridgelabz.fundoonotes;
-
+/*
+ * purpose :implementing login,registration using springboot
+ * */
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
