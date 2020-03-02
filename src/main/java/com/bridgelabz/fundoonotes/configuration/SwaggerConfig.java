27a319deb9b659e2f0ad2b329034638b66b5ca94
yaminijyothi <yamini0234@gmail.com>
2020-03-02 18:19:07 +0530

    [yaminijyothi] refactor. configurations added.

--- src/main/java/com/bridgelabz/fundoonotes/configuration/SwaggerConfig.java
+++ src/main/java/com/bridgelabz/fundoonotes/configuration/SwaggerConfig.java
@@ -1,5 +1,5 @@
 package com.bridgelabz.fundoonotes.configuration;
-
+//swagger configuration
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
--- src/main/java/com/bridgelabz/fundoonotes/configuration/UserConfiguration.java
+++ src/main/java/com/bridgelabz/fundoonotes/configuration/UserConfiguration.java
@@ -1,13 +1,15 @@
 package com.bridgelabz.fundoonotes.configuration;
-
+/*user configuration for encryptpassword,modelmapper,javamailsender*/
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-public class PasswordConfiguration {
+public class UserConfiguration {
 	
 	@Bean
 	public  BCryptPasswordEncoder passwordEncoder() {
@@ -17,6 +19,10 @@ public class PasswordConfiguration {
 	@Bean                                   
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
+	}
+	@Bean
+	public JavaMailSenderImpl sender() {
+		return new JavaMailSenderImpl();
 	}
 
 	
