583a0aa64ebec64566f9af73f408152b2225fa30
yaminijyothi <yamini0234@gmail.com>
2020-03-02 18:39:38 +0530

    [yaminijyothi] refactor. repositories added.

--- src/main/java/com/bridgelabz/fundoonotes/repository/Repositoryimpl.java
+++ src/main/java/com/bridgelabz/fundoonotes/repository/Repositoryimpl.java
@@ -1,5 +1,5 @@
 package com.bridgelabz.fundoonotes.repository;
-
+//service repository implementation
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -14,55 +14,52 @@ import org.springframework.stereotype.Repository;
 import com.bridgelabz.fundoonotes.model.UserInfo;
 @Repository
 public class Repositoryimpl implements UserRepository{
-	
-@Autowired
-private EntityManager manager;
 
-@Override
-public UserInfo register(UserInfo info) {
-	Session session=manager.unwrap(Session.class);
-	session.saveOrUpdate(info);
-	return  info;
-}
+	@Autowired
+	private EntityManager manager;
 
-@Override
-public UserInfo getUser(String email) {
-	Session session=manager.unwrap(Session.class);
-	Query<UserInfo> query=session.createQuery("from UserInfo where email=:email");
-	query.setParameter("email",email);
-	return (UserInfo)query.uniqueResult();
-}
-
-@Override
-public UserInfo findUserById(long id) {
-	Session session=manager.unwrap(Session.class);
-	Query<UserInfo> query=session.createQuery("from UserInfo where id=:id");
-	query.setParameter("userId", id);
-	return (UserInfo) query.uniqueResult();
-		
-}
-
-@Override
-public List<UserInfo> users() {
-	Session session=manager.unwrap(Session.class);
-	List<UserInfo> list=session.createQuery("from UserInfo").getResultList();
-	return list;
-}
-
-@Override
-public boolean verify(long userId) {
-	Session session=manager.unwrap(Session.class);
-	Query<UserInfo> query=session.createQuery("update UserInfo set is_verified=:q where userId=:i");
-	query.setParameter("q", true);
-	query.setParameter("i", userId);
-	int i=query.executeUpdate();
-	if(i>0) {
-		return true;
+	//**query for save the user data
+	@Override
+	public UserInfo register(UserInfo info) {
+		Session session=manager.unwrap(Session.class);
+		session.saveOrUpdate(info);
+		return  info;
 	}
-	return false;
 
-}
-}
-
-
+	//**query to find  user information from user email
+	@Override
+	public UserInfo getUser(String email) {
+		Session session=manager.unwrap(Session.class);
+		@SuppressWarnings("unchecked")
+		Query<UserInfo> query=session.createQuery("from UserInfo where email=:email");
+		query.setParameter("email",email);
+		return (UserInfo)query.uniqueResult();
+	}
 	
+	//**query to get all users
+	@Override
+	public List<UserInfo> users() {
+		Session session=manager.unwrap(Session.class);
+		@SuppressWarnings("unchecked")
+		List<UserInfo> list=session.createQuery("from UserInfo").getResultList();
+		return list;
+	}
+
+	//**query to verify user
+	@Override
+	public boolean verify(int userId) {
+		Session session=manager.unwrap(Session.class);
+		@SuppressWarnings("unchecked")
+		Query<UserInfo> query=session.createQuery("update UserInfo set isVerified=true where userId=:i");
+		query.setParameter("i", userId);
+		int i=query.executeUpdate();
+		if(i>0) {
+			return true;
+		}
+		return false;
+
+	}
+}
+
+
+
--- src/main/java/com/bridgelabz/fundoonotes/repository/UserRepository.java
+++ src/main/java/com/bridgelabz/fundoonotes/repository/UserRepository.java
@@ -5,18 +5,16 @@ import java.util.List;
 import com.bridgelabz.fundoonotes.dto.LoginDto;
 import com.bridgelabz.fundoonotes.dto.RegDto;
 import com.bridgelabz.fundoonotes.model.UserInfo;
-
+//service repository
 public interface UserRepository {
 
 	UserInfo  register(UserInfo  info);
-
-	UserInfo findUserById(long id);
 
 	UserInfo getUser(String email);
 
 	List<UserInfo> users();
 
-	boolean verify(long userId);
+	boolean verify(int userId);
 
 
 }
