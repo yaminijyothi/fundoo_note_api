8ea7783fc5b7927b54b8da2e4058301b6e963dfa
yaminijyothi <yamini0234@gmail.com>
2020-03-05 18:27:23 +0530

    [yaminijyothi] refractor. comments added.

--- src/main/java/com/bridgelabz/fundoonotes/controller/NoteController.java
+++ src/main/java/com/bridgelabz/fundoonotes/controller/NoteController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bridgelabz.fundoonotes.dto.NoteDto;
@@ -30,55 +29,41 @@ public class NoteController {
 	private NoteService service;
 
 	// API for create note
-	@PostMapping("/note/createnote")
-	public ResponseEntity<StatusRes> create(@RequestBody NoteDto data,@RequestHeader("token") String token) {
+	@PostMapping("/note/createnote/{token}")
+	public ResponseEntity<StatusRes> create(@RequestBody NoteDto data,@PathVariable("token") String token) {
 		Notes note =service.createNote(data, token);
-		if (note!=null) {
 			return ResponseEntity.status(HttpStatus.CREATED).body(new StatusRes("successfully created notes", 200, data));
-		}
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new StatusRes("failed to created notes", 400, data));
+
 	}
 	// API for update note
-	@PutMapping("/note/update")
-	public ResponseEntity<StatusRes> update(@RequestBody NoteUpdate data,@RequestHeader("token") String token ) {
+	@PutMapping("/note/update/{token}")
+	public ResponseEntity<StatusRes> update(@RequestBody NoteUpdate data,@PathVariable("token") String token ) {
 		Notes note =service.updateNote(data, token);
-		if (note!=null) {
-			return ResponseEntity.status(HttpStatus.CREATED).body(new StatusRes("successfully updated notes", 200, data));
-		}
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new StatusRes("failed to updated notes", 400, data));
+		return ResponseEntity.status(HttpStatus.CREATED).body(new StatusRes("successfully updated notes", 200, data));
 	}
 	// API for delete note
-	@DeleteMapping("/note/deletenote{id}")
-	public ResponseEntity<StatusRes> delete(@PathVariable long id,@RequestHeader("token") String token) {
+	@DeleteMapping("/note/deletenote/{id}/{token}")
+	public ResponseEntity<StatusRes> delete(@PathVariable long id,@PathVariable("token") String token) {
 		Notes note =service.deleteNote(id, token);		
-		if (note!=null) {
-			return ResponseEntity.status(HttpStatus.CREATED).body(new StatusRes("successfully deleted notes", 200));
-		}
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new StatusRes(" notes are not deleted", 400));
+			return ResponseEntity.status(HttpStatus.OK).body(new StatusRes("successfully deleted notes", 200));
 	}
 	//API for getting all notes
 	@GetMapping("/note/allnotes")
 	public ResponseEntity<StatusRes> getAllnotes() {
 		List<Notes> list =service.getAllNotes();
-		return ResponseEntity.status(HttpStatus.CREATED).body(new StatusRes("all notes", 200,list));
+		return ResponseEntity.status(HttpStatus.OK).body(new StatusRes("all notes", 200,list));
 	}
 	//API for get notes by userid
-	@GetMapping("/note/getNotebyUserId{token}")
+	@GetMapping("/note/getNotebyUserId/{token}")
 	public ResponseEntity<StatusRes> getNoteByUserId(@PathVariable("token") String token) {
 		List<Notes> list=service.getNoteByUserId(token);
-		if (list!=null) {
-			return ResponseEntity.status(HttpStatus.CREATED).body(new StatusRes("notes of particular user", 200,list));
-		}
-		return null;
+		return ResponseEntity.status(HttpStatus.OK).body(new StatusRes("notes of particular user", 200,list));
 	}
 	//API for getting singlenote
-	@GetMapping("/note/getNote{id}")
+	@GetMapping("/note/getNote/{id}")
 	public ResponseEntity<StatusRes> getNote(@PathVariable("id") long noteId) {
 		Notes note=service.getNote(noteId);
-		if (note!=null) {
-			return ResponseEntity.status(HttpStatus.CREATED).body(new StatusRes("notes based on note id", 200,note));
-		}
-		return null;
+			return ResponseEntity.status(HttpStatus.OK).body(new StatusRes("notes based on note id", 200,note));
 	}
 
 
