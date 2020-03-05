2f11500915e846e81200baaad65383dce8ac6573
yaminijyothi <yamini0234@gmail.com>
2020-03-05 18:24:48 +0530

    [yaminijyothi] refractor. configuration added.

--- src/main/java/com/bridgelabz/fundoonotes/configuration/UserConfiguration.java
+++ src/main/java/com/bridgelabz/fundoonotes/configuration/UserConfiguration.java
@@ -3,10 +3,8 @@ package com.bridgelabz.fundoonotes.configuration;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class UserConfiguration {
