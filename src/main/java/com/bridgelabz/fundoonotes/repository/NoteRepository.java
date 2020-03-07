122e22f2c2223634c476f7141c010f7fc8628f18
yaminijyothi <yamini0234@gmail.com>
2020-03-07 15:46:18 +0530

    [yaminijyothi] refractor. adding queries.

--- src/main/java/com/bridgelabz/fundoonotes/repository/NoteRepository.java
+++ src/main/java/com/bridgelabz/fundoonotes/repository/NoteRepository.java
@@ -11,10 +11,14 @@ import com.bridgelabz.fundoonotes.model.Notes;
 public interface NoteRepository extends CrudRepository<Notes, Long> {
 	@Query(value=" select * from notes where note_id=?",nativeQuery = true)
 	 Notes findNoteById(long noteId);
-	@Query(value="select * from notes where info_user_id =?",nativeQuery = true)
+	@Query(value="select * from notes where user_id =?",nativeQuery = true)
 	List<Notes> findNoteByUserId(int  id);
 	@Query(value="select * from notes where is_trashed=1",nativeQuery=true)
 	List<Notes> getTrashed(int id);
+	@Query(value="select * from notes where is_pinned=1",nativeQuery=true)
+	List<Notes> getPinned(int userId);
+	@Query(value="select * from notes where is_archieved=1",nativeQuery=true)
+	List<Notes> getArchieved(int userId);
  
 
 }
