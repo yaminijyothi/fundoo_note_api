77866d328268934905e14822ad47fd05c011668a
yaminijyothi <yamini0234@gmail.com>
2020-03-07 15:48:08 +0530

    [yaminijyothi] refractor. adding functions.

--- src/main/java/com/bridgelabz/fundoonotes/service/NoteService.java
+++ src/main/java/com/bridgelabz/fundoonotes/service/NoteService.java
@@ -9,12 +9,28 @@ import com.bridgelabz.fundoonotes.model.Notes;
 public interface NoteService {
 	
 	Notes createNote(NoteDto data,String token);
-	Notes updateNote(NoteUpdate data,String token);
+	
+	Notes updateNote(NoteUpdate data,String token,long noteId);
+	
 	Notes deleteNote(long NoteId,String token);
+	
 	List<Notes> getAllNotes();
+	
 	List<Notes> getNoteByUserId(String token);
+	
 	Notes getNote(long noteId);
+	
 	List<Notes> getTrashed(String token);
+	
+	Notes pinned(String token, long noteId);
+	
+	List<Notes> getPinned(String token);
+	
+	List<Notes> getArchieved(String token);
+	
+	Notes archieved(String token, long noteId);
+	
+	Notes addColour(String token, long noteId,String colour);
 	
 }
 
