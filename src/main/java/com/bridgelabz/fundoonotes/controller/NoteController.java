f2dca78b156978a5bf071fa21575bc7a397446f8
yaminijyothi <yamini0234@gmail.com>
2020-03-07 15:39:35 +0530

    [yaminijyothi] refractor. adding some functions.

--- src/main/java/com/bridgelabz/fundoonotes/controller/NoteController.java
+++ src/main/java/com/bridgelabz/fundoonotes/controller/NoteController.java
@@ -32,20 +32,19 @@ public class NoteController {
 	@PostMapping("/note/createnote/{token}")
 	public ResponseEntity<StatusRes> create(@RequestBody NoteDto data,@PathVariable("token") String token) {
 		Notes note =service.createNote(data, token);
-			return ResponseEntity.status(HttpStatus.CREATED).body(new StatusRes("successfully created notes", 200, note));
-
+		return ResponseEntity.status(HttpStatus.CREATED).body(new StatusRes("successfully created notes", 200, note));
 	}
 	// API for update note
-	@PutMapping("/note/update/{token}")
-	public ResponseEntity<StatusRes> update(@RequestBody NoteUpdate data,@PathVariable("token") String token ) {
-		Notes note =service.updateNote(data, token);
+	@PutMapping("/note/update/{token}/{noteId}")
+	public ResponseEntity<StatusRes> update(@RequestBody NoteUpdate data,@PathVariable("token") String token,@PathVariable("noteId")long noteId ) {
+		Notes note =service.updateNote(data, token,noteId);
 		return ResponseEntity.status(HttpStatus.CREATED).body(new StatusRes("successfully updated notes", 200,note));
 	}
 	// API for delete note
 	@DeleteMapping("/note/deletenote/{id}/{token}")
 	public ResponseEntity<StatusRes> delete(@PathVariable long id,@PathVariable("token") String token) {
 		Notes note =service.deleteNote(id, token);		
-			return ResponseEntity.status(HttpStatus.OK).body(new StatusRes("successfully deleted notes", 200));
+		return ResponseEntity.status(HttpStatus.OK).body(new StatusRes("successfully deleted notes", 200));
 	}
 	//API for getting all notes
 	@GetMapping("/note/allnotes")
@@ -63,15 +62,47 @@ public class NoteController {
 	@GetMapping("/note/getNote/{id}")
 	public ResponseEntity<StatusRes> getNote(@PathVariable("id") long noteId) {
 		Notes note=service.getNote(noteId);
-			return ResponseEntity.status(HttpStatus.OK).body(new StatusRes("notes based on note id", 200,note));
+		return ResponseEntity.status(HttpStatus.OK).body(new StatusRes("notes based on note id", 200,note));
 	}
 	//API for getting trashed notes
 	@GetMapping("note/trashednotes/{token}")
 	public ResponseEntity<StatusRes> getTrashedNotes(@PathVariable("token")String token){
 		List<Notes> note=service.getTrashed(token);
 		return ResponseEntity.status(HttpStatus.ACCEPTED).body(new StatusRes("trashed notes", 200,note));
-		
 	} 
+	//API for put pin to notes
+	@PutMapping("note/wantToPin/{token}/{noteId}")
+	public ResponseEntity<StatusRes> pinned(@PathVariable("token")String token,@PathVariable("noteId")long noteId){
+		Notes note = service.pinned(token,noteId);
+		return ResponseEntity.status(HttpStatus.ACCEPTED).body(new StatusRes("notes get pinned", 200));
+
+	}
+	//API for getting pinned notes
+	@GetMapping("note/getPinnedNotes/{token}")
+	public ResponseEntity<StatusRes> getPinnedNotes(@PathVariable("token")String token){
+		List<Notes> note=service.getPinned(token);
+		return ResponseEntity.status(HttpStatus.ACCEPTED).body(new StatusRes("pinned notes", 200,note));
+	}
+	//API for acrchieve notes
+	@PutMapping("note/wantToArchieve/{token}/{noteId}")
+	public ResponseEntity<StatusRes> archieved(@PathVariable("token")String token,@PathVariable("noteId")long noteId){
+		Notes note = service.archieved(token,noteId);
+		return ResponseEntity.status(HttpStatus.ACCEPTED).body(new StatusRes("notes get archieved", 200,note));
+	}
+	//API for getting archieved notes
+	@GetMapping("note/getArchievedNotes/{token}")
+	public ResponseEntity<StatusRes> getAchievedNotes(@PathVariable("token")String token){
+		List<Notes> note=service.getArchieved(token);
+		return ResponseEntity.status(HttpStatus.ACCEPTED).body(new StatusRes("Archieved notes", 200,note));
+	}
+	//API for adding colour to notes
+	@PutMapping("note/addColour/{token}/{noteId}/{colour}")
+	public ResponseEntity<StatusRes> addColour(@PathVariable("token")String token,@PathVariable("noteId")long noteId,@PathVariable("colour")String colour){
+		Notes note = service.addColour(token,noteId,colour);
+		return ResponseEntity.status(HttpStatus.ACCEPTED).body(new StatusRes("colour added successfully", 200,note));
+
+	}
+
 
 
 
