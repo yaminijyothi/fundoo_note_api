bfc4882d67f05306a83e97519b85146058a9e0ef
yaminijyothi <yamini0234@gmail.com>
2020-03-12 09:56:10 +0530

    [yaminijyothi] refractor. lable controller added.

--- src/main/java/com/bridgelabz/fundoonotes/controller/LableController.java
+++ src/main/java/com/bridgelabz/fundoonotes/controller/LableController.java
@@ -13,14 +13,11 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bridgelabz.fundoonotes.dto.LableDto;
-import com.bridgelabz.fundoonotes.dto.NoteDto;
 import com.bridgelabz.fundoonotes.model.Lables;
-import com.bridgelabz.fundoonotes.model.Notes;
 import com.bridgelabz.fundoonotes.response.StatusRes;
 import com.bridgelabz.fundoonotes.service.LableService;
 @RestController
@@ -31,14 +28,14 @@ public class LableController {
 
 	//API for creating lables
 	@PostMapping("lable/createLable/{token}")
-	public ResponseEntity<StatusRes> createLable(@RequestBody LableDto data,@PathVariable("token") String token){
+	public ResponseEntity<StatusRes> createLable(@RequestBody LableDto data,@PathVariable String token){
 		Lables lab=service.createLable(data, token);
 		return ResponseEntity.status(HttpStatus.CREATED).body(new StatusRes("lable created",200,lab));
 	}
 	//API for deleting lable
 	@DeleteMapping("lable/deleteLable/{LableId}/{token}")
-	public ResponseEntity<StatusRes> deleteLable(@PathVariable("LableId")long LableId,@PathVariable("token")String token){
-		service.deleteLable(LableId, token);
+	public ResponseEntity<StatusRes> deleteLable(@PathVariable long lableId,@PathVariable("token")String token){
+		service.deleteLable(lableId, token);
 		return ResponseEntity.status(HttpStatus.OK).body(new StatusRes("lable deleted",200));
 	}
 	//API for getting all lables
@@ -49,29 +46,41 @@ public class LableController {
 	}
 	//API for getting lable by userId
 	@GetMapping("lable/getLablebyUserId/{token}")
-	public ResponseEntity<StatusRes> getLableUserId(@PathVariable("token")String token){
+	public ResponseEntity<StatusRes> getLableUserId(@PathVariable String token){
 		List<Lables> lab=service.getLableByUserId(token);
 		return ResponseEntity.status(HttpStatus.CREATED).body(new StatusRes("lables of given user",200,lab));
 	}
 	//API for getting single lable
 	@GetMapping("lable/getSingleLable/{LableId}")
-	public ResponseEntity<StatusRes> getLable(@PathVariable("LableId")long LableId){
+	public ResponseEntity<StatusRes> getLable(@PathVariable long LableId){
 		Lables lab=service.getLable(LableId);
 		return ResponseEntity.status(HttpStatus.CREATED).body(new StatusRes("single lable",200,lab));
 	}
 	//API for mappping the notes and lables
-	@GetMapping("lable/getLableMap/{lableId}/{noteId}/{token}")
-	public ResponseEntity<StatusRes> addLable(@PathVariable("lableId")long lableId,@PathVariable("noteId")long  noteId,@PathVariable("token")String token){
+	@GetMapping("lable/LableMapwithNotes/{lableId}/{noteId}/{token}")
+	public ResponseEntity<StatusRes> addLable(@PathVariable long lableId,@PathVariable long  noteId,@PathVariable String token){
 		Lables lab=service.addLable(lableId,noteId,token);
-		return ResponseEntity.status(HttpStatus.CREATED).body(new StatusRes("single lable",200,lab));
+		return ResponseEntity.status(HttpStatus.CREATED).body(new StatusRes("mapped lable with note",200,lab));
 	}
-	//API for getting notes of lable
-	@GetMapping("lable/getNotesOfLable/{token}/{lableId}")
-	public ResponseEntity<StatusRes>  getNotesbyLableId(@PathVariable("token")String token,@PathVariable("lableId")long lableId){
-		List<Notes> list=service.getNotes(token,lableId);
-		return ResponseEntity.status(HttpStatus.CREATED).body(new StatusRes("single lable",200,list));
+	//API for sorting lables in ascending order
+	@GetMapping("lable/ascendingSort")
+	public ResponseEntity<StatusRes> ascendingSort(){
+		List<String> list=service.ascendingSort();
+		if(list!=null) {
+		return ResponseEntity.status(HttpStatus.ACCEPTED).body(new StatusRes("all lables in ascending sort order",200,list));
+		}
+		return ResponseEntity.status(HttpStatus.BAD_GATEWAY).body(new StatusRes("sorting failed",400,list));
 	}
-
+	//API for sorting lables in descending order
+	@GetMapping("lable/descendingSort")
+	public ResponseEntity<StatusRes> descendingSort(){
+		List<String> list=service.descendingSort();
+		if(list!=null) {
+		return ResponseEntity.status(HttpStatus.ACCEPTED).body(new StatusRes("all lables in descending sort order",200,list));
+		}
+		return ResponseEntity.status(HttpStatus.BAD_GATEWAY).body(new StatusRes("sorting failed",400,list));
+	}
+	
 }
 
 
