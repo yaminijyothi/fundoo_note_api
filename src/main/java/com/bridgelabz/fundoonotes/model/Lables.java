22ecc032f52f82d898c5058363f8c238c90550f7
yaminijyothi <yamini0234@gmail.com>
2020-03-12 10:06:39 +0530

    [yaminijyothi] refractor. lable entity added.

--- src/main/java/com/bridgelabz/fundoonotes/model/Lables.java
+++ src/main/java/com/bridgelabz/fundoonotes/model/Lables.java
@@ -1,25 +1,21 @@
 package com.bridgelabz.fundoonotes.model;
 
-import java.util.List;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
-
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
 @Data
+@NoArgsConstructor
+@ToString
 public class Lables {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long lableId;
 	private String name;
-	private int userId;
-	
-	@ManyToMany()
-	private List<Notes> notesList;
 	
 }
