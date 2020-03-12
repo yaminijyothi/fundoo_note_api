51ee17389490d260075df2f273d4ca30f55d0af6
yaminijyothi <yamini0234@gmail.com>
2020-03-12 09:22:19 +0530

    [yaminijyothi] refractor. userinfo added.

--- src/main/java/com/bridgelabz/fundoonotes/model/UserInfo.java
+++ src/main/java/com/bridgelabz/fundoonotes/model/UserInfo.java
@@ -9,13 +9,24 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Table(name="UserInfo")
 public class UserInfo {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
@@ -28,13 +39,14 @@ public class UserInfo {
 	private LocalDateTime date;	
 
 	@ManyToMany
-	private List<Notes> collabrator;
-	
-	@OneToMany(cascade = CascadeType.ALL,targetEntity = Notes.class)
+	@JoinTable(name="user_collaborator",joinColumns = {@JoinColumn(name="user_id")},inverseJoinColumns = {@JoinColumn(name="collaborator_note_id")})
+	private List<Notes> collabratorNote;
+
+	@OneToMany(targetEntity = Notes.class,cascade = CascadeType.ALL)
 	@JoinColumn(name="userId")
 	private List<Notes> notes;
-	
-	
-	
-	
+
+	@OneToMany(targetEntity = Lables.class,cascade = CascadeType.ALL)
+	@JoinColumn(name="userId")
+	private List<Lables> lables;
 }
