051701f7ccf3a2b4eeaf2f3ae8406f746fe6a42f
yaminijyothi <yamini0234@gmail.com>
2020-03-14 15:22:18 +0530

    [yaminijyothi] refractor. modified.

--- src/main/java/com/bridgelabz/fundoonotes/model/UserInfo.java
+++ src/main/java/com/bridgelabz/fundoonotes/model/UserInfo.java
@@ -9,12 +9,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
