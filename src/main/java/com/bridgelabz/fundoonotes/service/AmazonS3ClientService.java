7c1229f98894f8283bb379b834845ef95d911b17
yaminijyothi <yamini0234@gmail.com>
2020-03-14 15:42:04 +0530

    [yaminijyothi] add. amazon service added.

---
+++ src/main/java/com/bridgelabz/fundoonotes/service/AmazonS3ClientService.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.fundoonotes.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface AmazonS3ClientService {
+	
+	 void uploadFileToS3Bucket(MultipartFile multipartFile, boolean enablePublicReadAccess);
+
+	    void deleteFileFromS3Bucket(String fileName);
+}
